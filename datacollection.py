d85bb6381aba079afccfc735b0bd9717e572c545
Hardik Gandhi <62399245+hardikgandhi03@users.noreply.github.com>
2021-03-18 11:25:12 +0530

    Add files via upload

--- datacollection.py
+++ datacollection.py
@@ -1,6 +1,6 @@
 import cv2
-import numpy as np
 import os
+
 if not os.path.exists("dataset"):
     os.makedirs("dataset")
 if not os.path.exists("dataset/train"):
@@ -14,79 +14,75 @@ for i in range(10):
     if not os.path.exists("dataset/test/" + str(i)):
         os.makedirs("dataset/test/" + str(i))
 
-mode = 'train'
-directory = 'dataset/'+mode+'/'
-cap = cv2.VideoCapture(0)
-#interrupt = -1
+mode = 'test'
+folder = 'dataset/'+mode+'/'
+cam = cv2.VideoCapture(0)
+
 while True:
-    _, frame = cap.read()
+    _, frame = cam.read()
     frame = cv2.flip(frame, 1)
-
-    count = {
-        'zero': len(os.listdir(directory + "0")),
-        'one': len(os.listdir(directory + "1")),
-        'two': len(os.listdir(directory + "2")),
-        'three': len(os.listdir(directory + "3")),
-        'four': len(os.listdir(directory + "4")),
-        'five': len(os.listdir(directory + "5")),
-        'six': len(os.listdir(directory + "6")),
-        'seven': len(os.listdir(directory + "7")),
-        'eight': len(os.listdir(directory + "8")),
-        'nine': len(os.listdir(directory + "9")),
-
+    no_of_img = {
+        'zero': len(os.listdir(folder + "/0")),
+        'one': len(os.listdir(folder + "/1")),
+        'two': len(os.listdir(folder + "/2")),
+        'three': len(os.listdir(folder + "/3")),
+        'four': len(os.listdir(folder + "/4")),
+        'five': len(os.listdir(folder + "/5")),
+        'six': len(os.listdir(folder + "/6")),
+        'seven': len(os.listdir(folder + "/7")),
+        'eight': len(os.listdir(folder + "/8")),
+        'nine': len(os.listdir(folder + "/9")),
     }
-    cv2.putText(frame, "Zero : "+str(count['zero']), (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-    cv2.putText(frame, "One : "+str(count['one']), (10, 70), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-    cv2.putText(frame, "Two : "+str(count['two']), (10, 90), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-    cv2.putText(frame, "Three : "+str(count['three']), (10, 110), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-    cv2.putText(frame, "Four : "+str(count['four']), (10, 130), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-    cv2.putText(frame, "Five : "+str(count['five']), (10, 150), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-    cv2.putText(frame, "Six : "+str(count['six']), (10, 170), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-    cv2.putText(frame, "Seven : "+str(count['seven']), (10, 190), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-    cv2.putText(frame, "Eight : "+str(count['eight']), (10, 210), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
-    cv2.putText(frame, "Nine : "+str(count['nine']), (10, 230), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+    cv2.putText(frame, "Zero: "+str(no_of_img['zero']), (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+    cv2.putText(frame, "One: "+str(no_of_img['one']), (10, 70), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+    cv2.putText(frame, "Two: "+str(no_of_img['two']), (10, 90), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+    cv2.putText(frame, "Three: " + str(no_of_img['three']), (10, 110), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+    cv2.putText(frame, "Four: " + str(no_of_img['four']), (10, 130), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+    cv2.putText(frame, "Five: " + str(no_of_img['five']), (10, 150), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+    cv2.putText(frame, "Six: " + str(no_of_img['six']), (10, 170), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+    cv2.putText(frame, "Seven: " + str(no_of_img['seven']), (10, 190), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+    cv2.putText(frame, "Eight: " + str(no_of_img['eight']), (10, 210), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+    cv2.putText(frame, "Nine: " + str(no_of_img['nine']), (10, 230), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
     # print(frame.shape)
-    # x1 = int(0.5*frame.shape[1])
-    # x2 = frame.shape[1]-10
-    # y1 = 10
-    # y2 = int(0.5 * frame.shape[1])
-    # # print("{},{},{},{}".format(x1,y1,x2,y2)) Ans:- 320,10,630,320
-    cv2.rectangle(frame,(320,10),(620,320),(0,255,0),1)
-    roi=frame[10:320,320:620]
+    x1 = int(0.5*frame.shape[1])
+    x2 = frame.shape[1]-10
+    y1 = 10
+    y2 = int(0.5 * frame.shape[1])
+    # print("{},{},{},{}".format(x1,y1,x2,y2)) Ans:- 320,10,630,320
+    cv2.rectangle(frame,(319,9),(620+1,309),(0,255,0),1)
+    roi=frame[10:300,320:620]
+
     cv2.imshow("Frame", frame)
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     gaussblur = cv2.GaussianBlur(gray,(5,5),2)
     smallthres = cv2.adaptiveThreshold(gaussblur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,9,2.8)
     ret, final_image = cv2.threshold(smallthres, 70, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
     final_image = cv2.resize(final_image, (300, 300))
     cv2.imshow("BW", final_image)
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
-    interrupt = cv2.waitKey(10)
 
-    if interrupt == 27:  # esc key
-        break
+    interrupt = cv2.waitKey(1)
     if interrupt == ord('0'):
-        cv2.imwrite('dataset/train/0/' + str(count['zero']) + '.jpg', final_image)
+        cv2.imwrite(folder + '0/' + str(no_of_img['zero']) + '.jpg', final_image)
     if interrupt == ord('1'):
-        cv2.imwrite(directory + '1/' + str(count['one']) + '.jpg', final_image)
+        cv2.imwrite(folder + '1/' + str(no_of_img['one']) + '.jpg', final_image)
     if interrupt == ord('2'):
-        cv2.imwrite(directory + '2/' + str(count['two']) + '.jpg', final_image)
+        cv2.imwrite(folder + '2/' + str(no_of_img['two']) + '.jpg', final_image)
     if interrupt == ord('3'):
-        cv2.imwrite(directory+'3/'+str(count['three'])+'.jpg', final_image)
+        cv2.imwrite(folder +'3/'+str(no_of_img['three'])+'.jpg', final_image)
     if interrupt == ord('4'):
-        cv2.imwrite(directory+'4/'+str(count['four'])+'.jpg', final_image)
+        cv2.imwrite(folder +'4/'+str(no_of_img['four'])+'.jpg', final_image)
     if interrupt == ord('5'):
-        cv2.imwrite(directory+'5/'+str(count['five'])+'.jpg', final_image)
+        cv2.imwrite(folder +'5/'+str(no_of_img['five'])+'.jpg', final_image)
     if interrupt == ord('6'):
-        cv2.imwrite(directory+'6/'+str(count['six'])+'.jpg', final_image)
+        cv2.imwrite(folder +'6/'+str(no_of_img['six'])+'.jpg', final_image)
     if interrupt == ord('7'):
-        cv2.imwrite(directory+'7/'+str(count['seven'])+'.jpg', final_image)
+        cv2.imwrite(folder +'7/'+str(no_of_img['seven'])+'.jpg', final_image)
     if interrupt == ord('8'):
-        cv2.imwrite(directory+'8/'+str(count['eight'])+'.jpg', final_image)
+        cv2.imwrite(folder +'8/'+str(no_of_img['eight'])+'.jpg', final_image)
     if interrupt == ord('9'):
-        cv2.imwrite(directory+'9/'+str(count['nine'])+'.jpg', final_image)
-
-cap.release()
+        cv2.imwrite(folder +'9/'+str(no_of_img['nine'])+'.jpg', final_image)
+    if interrupt  == ord('q'):
+        break
+cam.release()
 cv2.destroyAllWindows()
